505abd11902a7c0c4c8bf2567e4bc6ae371ebeb1
Alvaro Videla <alvaro.videla@fauna.com>
2018-07-25 23:00:49 +0200

    bumps version to 0.2.3

--- fauna-shell.rb
+++ fauna-shell.rb
@@ -3,9 +3,9 @@ require "language/node"
 class FaunaShell < Formula
   desc "Interactive shell for FaunaDB"
   homepage "https://fauna.com/"
-  url "https://registry.npmjs.org/fauna-shell/-/fauna-shell-0.2.2.tgz"
-  sha256 "c8e5f48b9dc1675d0c3a8db58426defd005f2de27de7b3c0aa8cc397fce54bd9"
-  version "0.2.2"
+  url "https://registry.npmjs.org/fauna-shell/-/fauna-shell-0.2.3.tgz"
+  sha256 "97614c71116ca8aa5fc2494a3176df44d301ea85437ad4974e8a65ffde2517ef"
+  version "0.2.3"
   
   depends_on "node"
 
