875e66a33ea74d62b52b4aa7a443f61e1cd9fed7
Alvaro Videla <alvaro.videla@fauna.com>
2018-08-03 15:27:45 +0200

    fixes whitespace

--- fauna-shell.rb
+++ fauna-shell.rb
@@ -6,14 +6,14 @@ class FaunaShell < Formula
   url "https://registry.npmjs.org/fauna-shell/-/fauna-shell-0.3.0.tgz"
   sha256 "e587255300cfd8e8be5afa5d9497bdb612eeeca5e165d65bd05fbd2e041ec840"
   version "0.3.0"
-  
+
   depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
-  
+
   test do
     system bin/"fauna", "-O", "/dev/null", "--help"
   end
