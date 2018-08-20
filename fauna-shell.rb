f0fdd86cb9b9eee5738f498b398cbcac546b4e97
Alvaro Videla <alvaro.videla@fauna.com>
2018-08-20 14:53:52 +0200

    updates the formula to match what's on Hombrew\'s core

--- fauna-shell.rb
+++ fauna-shell.rb
@@ -6,6 +6,13 @@ class FaunaShell < Formula
   url "https://registry.npmjs.org/fauna-shell/-/fauna-shell-0.5.0.tgz"
   sha256 "b365080e638d85104a9ae533c6271c2dae692a5d6cc09f7fdb15d5c8870a96aa"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "38aa9051550b380601f151595a0bf5cfe5b379bcc2ca698185f42c8203941839" => :high_sierra
+    sha256 "11f3fcdf64f00f374af9a3dc2812fc1d99178c12146a9d8bc496496be1ab4190" => :sierra
+    sha256 "44b0dc90d6d6ffaee55ebe7dd569d540f388ffaa5c15a87e1991a355d58ddc81" => :el_capitan
+  end
+
   depends_on "node"
 
   def install
@@ -14,53 +21,12 @@ class FaunaShell < Formula
   end
 
   test do
-    # just making sure the program was properly installed
-    system bin/"fauna", "--help"
+    output = shell_output("#{bin}/fauna list-endpoints 2>&1", 1)
+    assert_match "No endpoints defined", output
 
-    require "open3"
+    pipe_output("#{bin}/fauna add-endpoint https://endpoint1:8443", "secret\nendpoint1\n")
 
-    # there are no endpoints, so an error is raised
-    _o, _e, s = Open3.capture3("#{bin}/fauna list-endpoints")
-    assert_equal false, s.success?
-
-    _o, _e, s = Open3.capture3("#{bin}/fauna add-endpoint https://endpoint1:8443", :stdin_data=>"secret\nendpoint1\n")
-    assert s.success?
-
-    # the endpoint 'endpoint1' was added and is listed as default (*)
-    o, _e, s = Open3.capture3("#{bin}/fauna list-endpoints")
-    assert_equal "endpoint1 *\n", o
-    assert s.success?
-
-    # by no providing an endpoint alias, it's taken from the hostname
-    _o, _e, s = Open3.capture3("#{bin}/fauna add-endpoint https://endpoint2:8443", :stdin_data=>"secret\n\n")
-    assert s.success?
-
-    # the endpoint 'endpoint2' was added, and 'endpoint1' is still listed as default (*)
-    o, _e, s = Open3.capture3("#{bin}/fauna list-endpoints")
-    assert_equal "endpoint1 *\nendpoint2\n", o
-    assert s.success?
-
-    # sets 'endpoint2' as the default endpoint
-    _o, _e, s = Open3.capture3("#{bin}/fauna default-endpoint endpoint2")
-    assert s.success?
-
-    # endpoint 'endpoint2' is now the default endpoint (*)
-    o, _e, s = Open3.capture3("#{bin}/fauna list-endpoints")
-    assert_equal "endpoint1\nendpoint2 *\n", o
-    assert s.success?
-
-    _o, _e, s = Open3.capture3("#{bin}/fauna delete-endpoint endpoint1", :stdin_data=>"y\n")
-    assert s.success?
-
-    o, _e, s = Open3.capture3("#{bin}/fauna list-endpoints")
-    assert_equal "endpoint2 *\n", o
-    assert s.success?
-
-    _o, _e, s = Open3.capture3("#{bin}/fauna delete-endpoint endpoint2", :stdin_data=>"y\n")
-    assert s.success?
-
-    # there are no endpoints, so an error is raised
-    _o, _e, s = Open3.capture3("#{bin}/fauna list-endpoints")
-    assert_equal false, s.success?
+    output = shell_output("#{bin}/fauna list-endpoints")
+    assert_equal "endpoint1 *\n", output
   end
 end
